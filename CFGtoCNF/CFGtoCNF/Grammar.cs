23a8fcc442c2bd4707b155cf40a888fdd18719b9
Dan3s <daniel.guzman1@correo.icesi.edu.co>
2019-11-08 18:09:24 -0500

    Creando interfaz

---
+++ CFGtoCNF/CFGtoCNF/Grammar.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace CFGtoCNF
+{
+    class Grammar
+    {
+
+        private Grammar actualGrammar;
+        private Dictionary<String,Variable> variables;
+        
+
+        public Grammar(){
+            variables = new Dictionary<String, Variable>();
+            actualGrammar = new Grammar();
+            
+        }
+
+         /**
+         * Entrada de ejemplo
+         * S abc
+         */
+        private void addVariables(String key, String pro){
+            String[] cad = pro.Split("/");
+            for(int i = 0; i<cad.Length; i++){
+                //variables.Add(key, cad[i]);
+            }
+            
+        }
+
+        //T AL AN U
+
+        public Variable[] Terminales() {
+            Dictionary<String,String>  terminales1 = new Dictionary<String,String>();
+            Dictionary<String,String>  terminales2 = new Dictionary<String,String>();
+
+            //bool equal = terminales1.Keys.Count == terminales2.Keys.Count && terminales1.Keys.All(k => terminales2.ContainsKey(k) && object.Equals(terminales2[k], terminales1[k]));
+ 
+            foreach(KeyValuePair<String, Variable> entry in variables)
+            {
+                Variable vari = entry.Value;
+                if(vari.haveTerminal()){
+                    terminales1.Add(entry.Key, entry.Key);
+                }
+                if(vari.haveLambda()){
+                    terminales1.Add(entry.Key, entry.Key);
+                }
+            }
+            for (int i = 0; i< variables.Count; i++) {
+                for (int j = 0; j < variables.Count; j++) {
+                   // variables[i]
+                }
+            }
+
+            return null;
+        }
+       
+
+        
+
+      
+
+
+
+
+
+    }
+}
---
+++ CFGtoCNF/CFGtoCNF/Variable.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Text;
+
+namespace CFGtoCNF
+{
+    class Variable
+    {
+        //En cada posicion una produccion de la forma aa ó AA ó aA ó A ó a ó lambda
+        private  StringCollection  producciones;
+
+         public bool haveTerminal()
+        {
+
+            for (int i = 0; i < producciones.Count; i++)
+            {
+                if (producciones[i].Length == 1 && isSmallLetter(producciones[i][0]))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        //Anulables
+        public bool haveLambda()
+        {
+
+            for (int i = 0; i < producciones.Count; i++)
+            {
+                if (producciones[i].Length == 1 && producciones[i].Equals("?")) {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        //Unitarias
+        public StringCollection Unitarias()
+        {
+			StringCollection unitarias = new StringCollection();
+
+            for (int i = 0; i < producciones.Count; i++)
+            {
+                if (producciones[i].Length == 1 && isBigLetter(producciones[i][0]))
+                {
+                    unitarias.Add(""+producciones[i][0]);
+                }
+            }
+            return unitarias;
+        }
+
+        //Alcanzables
+        //Retorna las alcanzables de la produccion
+        public StringCollection Reachables()
+        {
+            StringCollection alcanzables = new StringCollection();
+            for (int i = 0; i < producciones.Count; i++)
+            {
+                for (int j = 0; j < producciones[i].Length; j++)
+                {
+
+                    if (isBigLetter(producciones[i][j]) && !alcanzables.Contains(""+producciones[i][j]))
+                    {
+                        alcanzables.Add(""+producciones[i][j]);
+                    }
+                }
+            }
+            return alcanzables;
+        }
+
+
+
+
+
+
+        //Mayusculas/variables
+        public bool isBigLetter(char digit)
+        {
+
+            if (digit >= 65 && digit <= 90)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+
+        //Minusculas/terminables
+        // Lambda es '?'
+        public bool isSmallLetter(char digit)
+        {
+
+            if ((digit >= 97 && digit <= 122)|| digit == 63)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+		
+	//public static void Main()
+	//{
+	//	Console.WriteLine(isBigLetter('P'));
+	//	producciones = new StringCollection();
+	//	producciones.Add("ABC");
+	//	producciones.Add("BaC");
+	//	producciones.Add("aB");
+	//	producciones.Add("F");
+	//	producciones.Add("a");
+	//	producciones.Add("P");
+	//	producciones.Add("q");
+	//	Console.WriteLine(producciones.Count);
+	//	StringCollection bal = Reachables();
+	//	StringCollection lab = Unitarias();
+		
+	//	//for(int i = 0; i < bal.Count; i++){
+	//	//	Console.WriteLine(bal[i]);
+	//	//}
+	//	for(int i = 0; i < lab.Count; i++){
+	//		Console.WriteLine(lab[i]);
+	//	}
+		
+	//}
+
+    }
+}
